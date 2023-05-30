32b89cf901f8764f489a7218496ad7f7e17479d0
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-05-30 13:21:05 -0300

    exercicios-dia-02

---
+++ python-day-2-exercises/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+def generate_random_array():
+#     array = []
+#     for _ in range(20):
+#         number = random.randint(0, 100)
+#         array.append(number)
+#     return array
+
+
+# random_array = generate_random_array()
+# print(random_array)
